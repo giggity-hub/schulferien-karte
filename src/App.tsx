7a23bb0f78c28ad133b9e6f2288168b703617cd8
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-09-28 13:14:12 +0200

    added svg map

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ import Table from './Table'
 function App() {
   return (
     <div className='flex flex-col w-full bg-green-300 lg:flex-row'>
-      <div className='w-full h-screen bg-red-500'>
-        <div className="fixed lg:relative bg-orange-200 w-full h-full">
-          hello boiiiisss
+      <div className='h-screen bg-red-500'>
+        <div className="w-full fixed lg:relative bg-orange-200 h-full p-5">
+            <HolidayMap/>
         </div>
       </div>
-      <div className='-mt-20 lg:mt-0 w-full h-screen lg:overflow-y-auto bg-purple-200 relative z-index-100'>
+      <div className='flex-grow -mt-20 lg:mt-0 h-screen lg:overflow-y-auto bg-purple-200 relative z-index-100'>
         <div className='bg-green-200 h-500'>asdfasd</div>
       </div>
-      {/* <div className="mopedschmoped bg-blue-200 relative z-index-100 h-500" style={{marginTop: `calc(100vh - 10rem)`}} >tabbel</div>
-      <div className="bg-orange-500 h-screen fixed top-0 w-full">kardde</div> */}
     </div>
   )
 }
--- src/HolidayMap.tsx
+++ src/HolidayMap.tsx
@@ -1,7 +1,21 @@
+import * as d3 from 'd3'
+import bundeslaender from './bundeslaender.json'
+
 export default function HolidayMap(){
+
+    let width = 800
+    let height = 900
+    console.log(bundeslaender);
+    
+    let projection = d3.geoMercator().fitSize([width, height], bundeslaender)
+    let geoGenerator = d3.geoPath(projection)
+    
+
     return (
-        <div>
-            hello boiiiiiiiiiiiiiiiiiiiiis
-        </div>
+        <svg viewBox={`0 0 ${width} ${height}`} className="h-full mx-auto">
+            {bundeslaender.features.map(x => {
+                return <path stroke="green" fill="white" d={geoGenerator(x)}></path>
+            })}
+        </svg>
     )
 }
